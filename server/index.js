c572f0cb51ad4c882984d20568b89a63eef00215
Dean Ma <mrdingma@gmail.com>
2020-01-23 12:10:58 -0800

    serves static client directory

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
