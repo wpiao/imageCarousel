a34b6a02ed67dfb3830584623a931c67e78c3df0
Dean Ma <mrdingma@gmail.com>
2020-01-25 18:11:51 -0800

    seeds mongoDb

--- server/db/index.js
+++ server/db/index.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost:37017/gallery', { useNewUrlParser: true })
 const GallerySchema = mongoose.Schema({
   listing_id: { type: Number, required: true, unique: true },
   listing_title: { type: String },
-  listing_images: [{ url: String, description: String }],
+  listing_images: [{ id: Number, url: String, caption: String }],
 });
 
 module.exports = mongoose.model('Gallery', GallerySchema);
---
+++ server/db/seed.js
@@ -0,0 +1,31 @@
+const jsonData = require('./dummydata.js');
+const GalleryModel = require('./index.js');
+
+
+GalleryModel.deleteMany({})
+  .then((result) => console.log(`deleted count: ${result.deletedCount}`));
+
+let count = 1;
+
+const dataExtract = jsonData.map((obj) => {
+  const extract = {
+    listing_id: count,
+    listing_title: obj.pdp_listing_detail.name,
+    listing_images: obj.pdp_listing_detail.photos.map((photoObj) => ({
+      id: photoObj.id,
+      url: photoObj.picture,
+      caption: photoObj.caption,
+    })),
+  };
+  count += 1;
+
+  return extract;
+});
+
+GalleryModel.insertMany(dataExtract)
+  .then((docs) => {
+    console.log(`Successfully inserted ${docs.length} documents`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
